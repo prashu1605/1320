7d4405ac039c15f08d11dc555bb902df3b54a0b4
prashu1605 <158436071+prashu1605@users.noreply.github.com>
2026-02-17 19:11:42 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,28 @@
+package com.practicecode;
+
+public class Employee {
+	public static void main(String args[]) {
+		
+		String name = "Abhishek";
+		int employe_id = 21;
+		byte age = 23;
+		String Experience = "Abhishek";
+		String Qualification = "Btech-cs";
+		float salary = 9.5f;
+		long incentives = 5000l;
+		String Department = "Software-development";
+		
+		
+		System.out.println("Employee Name : " + name);
+		System.out.println("Employee id : " + employe_id);
+		System.out.println("Employee age : " + age);
+		System.out.println("Experience : " + Experience);
+		System.out.println("Employee Qualifications : " + Qualification);
+		System.out.println("Employee Salary : " + salary);
+		System.out.println("Employee Incentives : " + incentives);
+		System.out.println("Department : " + Department);
+		
+		
+	}
+
+}
